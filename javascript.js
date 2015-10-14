0148e832424f0291cee02d6befce7b15f43bc634
Alyssa Wilson <alyssa4327@gmail.com>
2015-10-14 18:03:05 -0500

    Javascript Day 1
    Javascript Day 1

---
+++ javascript.js
@@ -0,0 +1,66 @@
+
+//alert ("External JS Message")
+var name = "alyssa";
+console.log(name);
+console.log("alyssa");
+
+//var mynumber = 4000000000;
+//alert (mynumber);
+//console.log(mynumber);
+
+//Number 
+
+var x = 5;
+var y = 6;
+console.log (x+y);
+
+var a = 32;
+var b = 6;
+console.log (a+b);
+
+console.log (x*"3");
+console.log (a-b);
+console.log (x*a);
+
+//Strings 
+
+var vegetableName = "carrot"
+var fruitName = "strawberry"
+
+console.log (vegetableName);
+console.log (fruitName);
+
+console.log (fruitName + x);
+
+console.log (vegetableName + fruitName);
+console.log (fruitName + vegetableName);
+
+//Boolean 
+
+var a1 = Boolean(300);
+var a2 = Boolean(-15);
+
+console.log (a1)
+
+//Array 
+
+var x = [11, 12, 13]
+var y = [a, 31, "pizza"]
+
+console.log (x [0]);
+
+console.log (x[1] + y [1]);
+console.log (y[1] * x [0]);
+
+var z = [x * [1, 2, 3]];
+
+console.log (z [1]);
+
+console.log (x.length);
+
+
+
+
+
+
+
